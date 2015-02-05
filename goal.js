19a525df220293555929b84a217fa0a9cd20d097
Tim Perry <pimterry@gmail.com>
2015-02-05 10:55:41 +0000

    Add failing Chai helpers

--- goal.js
+++ goal.js
@@ -8,8 +8,37 @@ var driver = new sw.Builder()
 var chai = require('chai');
 var chaiWebdriver = require('chai-webdriver');
 chai.use(chaiWebdriver(driver));
+
+sizzle = require('webdriver-sizzle')
+var $ = sizzle(driver, sw);
+
+chai.use(function (chai, utils) {
+  chai.Assertion.addMethod('leftAligned', function(done) {
+    var self = this;
+    if (!utils.flag(this, 'dom')) {
+      throw new Error('Can only test alignment of dom elements');
+    }
+    return $.all(this._obj).then(function(els) {
+      self.assert(false, 'Should be left aligned');
+      return typeof done === "function" ? done() : void 0;
+    });      
+  });
+  chai.Assertion.addMethod('equalWidth', function(done) {
+    var self = this;
+    if (!utils.flag(this, 'dom')) {
+      throw new Error('Can only test size of dom elements');
+    }
+    return $.all(this._obj).then(function(els) {
+      self.assert(false, 'Should have equal width');
+      return typeof done === "function" ? done() : void 0;
+    });      
+  });
+});
  
 // And you're good to go! 
 driver.get('http://github.com');
-chai.expect('input[name="user[email]"], input[name="user[name]"], input[name="user[password]"], button.primary')
-    .dom.to.be.leftAligned().and.have.equalWidth()
+
+loginElements = 'input[name="user[email]"], input[name="user[name]"], input[name="user[password]"], button.primary'
+
+chai.expect(loginElements).dom.to.be.leftAligned()
+chai.expect(loginElements).dom.to.have.equalWidth()
